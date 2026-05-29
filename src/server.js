4b5ac953687ca97ca55a0c13f8430f0108264317
Sandeepraj M <157199000+sandeepraj-dev@users.noreply.github.com>
2026-05-29 17:54:52 +0530

    fixed port issue

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const connectDB = require("./config/db");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 connectDB();
 
