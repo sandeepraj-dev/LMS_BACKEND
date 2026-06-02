f1443311a1dc1e6b1f23e1cba254ba602bd186b1
Sandeepraj M <157199000+sandeepraj-dev@users.noreply.github.com>
2026-06-02 23:19:37 +0530

    fixed Student endpoint

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -1,6 +1,7 @@
 const Attempt = require("../models/Attempt");
 const Exam = require("../models/Exam");
 const Question = require("../models/Question");
+const User = require("../models/User");
 
 exports.attemptExam = async (req, res) => {
   try {
